cf443cb64ad2267628d7cd8775506dca7cce5795
Purva Shah <purvashah98@gmail.com>
2019-04-10 22:19:41 +0530

    Comitted changes

---
+++ epamproj3/src/epamproj3/solution.java
@@ -0,0 +1,74 @@
+package epamproj3;
+
+public class solution {
+	static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        SinglyLinkedListNode temporary_node = head;
+    
+        for(int i = 0; i < 100; i++) {
+            if(temporary_node == null) {
+                return false;
+            } else {
+                temporary_node = temporary_node.next;
+            }   
+        }   
+        return true;
+}
+    private static final Scanner scanner = new Scanner(System.in);
